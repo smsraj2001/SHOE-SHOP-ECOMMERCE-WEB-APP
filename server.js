79fbe744890a048fe460a2084f1a8029f1328967
S M SUTHARSAN RAJ <sutharsanraj2001@gmail.com>
2024-11-20 20:20:18 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.use('/api', orderRoutes);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  const path = require('path');
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
